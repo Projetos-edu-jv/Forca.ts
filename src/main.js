71f3f65c9bb79f606e66ff0079dd3a66ac7ea153
Jvsm123 <Jvsm123@outlook.com>
2022-01-03 18:03:29 -0300

    Starting Project

--- src/main.js
+++ src/main.js
@@ -1,1 +1,47 @@
-console.log('Olá Mundo');
+// console.log('Olá Mundo');
+;
+var Game = /** @class */ (function () {
+    function Game() {
+        this.palavra = this.GetPalavra();
+    }
+    ;
+    Game.prototype.Init = function () {
+        console.log("Jogo iniciado!");
+        this.Loop(); //Faz loop do jogo
+    };
+    ;
+    Game.prototype.Loop = function () {
+        console.log("Coisas que vão acontecer nesse jogo!");
+        var letra = this.GetLetra();
+        this.Verificador(letra);
+    };
+    ;
+    Game.prototype.GetLetra = function () {
+        console.log("Recebe Letra");
+        return "";
+    };
+    ;
+    Game.prototype.GetPalavra = function () {
+        console.log("Recebe Palavra");
+        return "";
+    };
+    ;
+    Game.prototype.Verificador = function (letra) {
+        console.log("Verifica letra com palavra");
+        letra === this.palavra
+            ? console.log("Venceu!")
+            : console.log("Perdeu!");
+        this.EndGame();
+    };
+    ;
+    Game.prototype.EndGame = function () {
+        console.log("Encerra loop");
+        console.log("Limpa o jogo");
+        console.log("cria nova palavra");
+        console.log("Espera main ser chamada denovo");
+    };
+    return Game;
+}());
+;
+var main = new Game;
+main.Init();
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,63 @@
-console.log('Olá Mundo')
+// console.log('Olá Mundo');
+
+interface System { readonly palavra: string };
+
+class Game implements System
+{
+	readonly palavra: string;
+ 
+	constructor() { this.palavra = this.GetPalavra() };
+ 
+	Init(): void
+	{
+		console.log("Jogo iniciado!");
+	 
+		this.Loop(); //Faz loop do jogo
+	};
+ 
+	private Loop(): void
+	{
+		console.log("Coisas que vão acontecer nesse jogo!");
+	 
+		const letra: string = this.GetLetra();
+	 
+		this.Verificador( letra )
+	};
+
+	private GetLetra(): string
+	{
+		console.log("Recebe Letra");
+	 
+		return "";
+	};
+
+	GetPalavra(): string
+	{
+		console.log("Recebe Palavra");
+
+		return "";
+	};
+
+	private Verificador( letra: string ): void
+	{
+		console.log("Verifica letra com palavra");
+	 
+		letra === this.palavra
+			? console.log("Venceu!")
+			: console.log("Perdeu!");
+
+		this.EndGame();
+	};
+
+	private EndGame(): void
+	{
+		console.log("Encerra loop");
+		console.log("Limpa o jogo");
+		console.log("cria nova palavra");
+		console.log("Espera main ser chamada denovo");
+	}
+};
+
+const main = new Game;
+
+main.Init();
